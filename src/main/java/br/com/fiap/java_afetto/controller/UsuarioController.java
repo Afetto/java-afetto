282b5ca0fb973f9694ab824bfbdf395a5201d8e1
leonardobarbosas <leonardobarbosasantos@gmail.com>
2026-05-18 13:27:32 -0300

    adicionando crud de Usuário

--- src/main/java/br/com/fiap/java_afetto/controller/UsuarioController.java
+++ src/main/java/br/com/fiap/java_afetto/controller/UsuarioController.java
@@ -1,17 +1,25 @@
 package br.com.fiap.java_afetto.controller;
 
+import br.com.fiap.java_afetto.dto.endereco.EnderecoLista;
+import br.com.fiap.java_afetto.dto.usuario.UsuarioLista;
 import br.com.fiap.java_afetto.dto.usuario.UsuarioRequest;
 import br.com.fiap.java_afetto.dto.usuario.UsuarioResponse;
 import br.com.fiap.java_afetto.service.UsuarioService;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/usuario")
@@ -29,5 +37,50 @@ public class UsuarioController {
     @PostMapping
     public ResponseEntity<UsuarioResponse> createUsuario(@Valid @RequestBody UsuarioRequest usuarioRequest){
         return ResponseEntity.status(HttpStatus.CREATED).body(usuarioService.create(usuarioRequest));
+    }
+
+    @Operation(summary = "Busca um usuário por id")
+    @GetMapping("/{id}")
+    public ResponseEntity<UsuarioResponse> readUsuario(@PathVariable UUID id) {
+        UsuarioResponse usuario = usuarioService.read(id);
+        if (usuario == null) {
+            return new ResponseEntity<>(usuario, HttpStatus.NO_CONTENT);
+        }
+        return new ResponseEntity<>(usuario, HttpStatus.OK);
+    }
+
+    @Operation(summary = "Busca todos os usuários por página")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200",
+                    description = "Página de usuários retornada com sucesso!",
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = EnderecoLista.class))),
+            @ApiResponse(responseCode = "404",
+                    description = "Nenhum usuário encontrado!",
+                    content = @Content(schema = @Schema()))
+    })
+    @GetMapping
+    public ResponseEntity<Page<UsuarioLista>> readUsuario(@RequestParam(defaultValue = "0") Integer pageNumber) {
+        Pageable pageable = PageRequest.of(pageNumber, 50);
+        Page<UsuarioLista> usuario = usuarioService.read(pageable);
+        if (usuario.isEmpty()) {
+            return  new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<>(usuario, HttpStatus.OK);
+    }
+
+    @Operation(summary = "Atualiza o usuário")
+    @PutMapping("/{id}")
+    public ResponseEntity<UsuarioResponse> updateUsuario(
+            @PathVariable UUID id,
+            @Valid @RequestBody UsuarioRequest request) {
+        return ResponseEntity.ok(usuarioService.update(id, request));
+    }
+
+    @Operation(summary = "Deleta o usuário")
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteUsuario(@PathVariable UUID id) {
+        usuarioService.delete(id);
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 }
--- src/main/java/br/com/fiap/java_afetto/service/UsuarioService.java
+++ src/main/java/br/com/fiap/java_afetto/service/UsuarioService.java
@@ -1,20 +1,14 @@
 package br.com.fiap.java_afetto.service;
 
-import br.com.fiap.java_afetto.dto.logradouro.LogradouroLista;
-import br.com.fiap.java_afetto.dto.logradouro.LogradouroRequest;
-import br.com.fiap.java_afetto.dto.logradouro.LogradouroResponse;
+
+import br.com.fiap.java_afetto.dto.usuario.UsuarioLista;
 import br.com.fiap.java_afetto.dto.usuario.UsuarioRequest;
 import br.com.fiap.java_afetto.dto.usuario.UsuarioResponse;
-import br.com.fiap.java_afetto.mapper.LogradouroMapper;
 import br.com.fiap.java_afetto.mapper.UsuarioMapper;
 import br.com.fiap.java_afetto.model.Usuario;
-import br.com.fiap.java_afetto.model.endereco.Bairro;
 import br.com.fiap.java_afetto.model.endereco.Endereco;
-import br.com.fiap.java_afetto.model.endereco.Logradouro;
 import br.com.fiap.java_afetto.repository.UsuarioRepository;
-import br.com.fiap.java_afetto.repository.endereco.BairroRepository;
 import br.com.fiap.java_afetto.repository.endereco.EnderecoRepository;
-import br.com.fiap.java_afetto.repository.endereco.LogradouroRepository;
 import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -40,9 +34,6 @@ public class UsuarioService {
 
 
     public UsuarioResponse create(UsuarioRequest usuarioRequest) {
-        Endereco endereco = enderecoRepository.findById(usuarioRequest.idEndereco())
-                .orElseThrow(() -> new EntityNotFoundException("Endereco não encontrado"));
-
         Usuario usuario = new Usuario();
         usuario.setNome(usuarioRequest.nome());
         usuario.setCpf(usuarioRequest.cpf());
@@ -50,35 +41,41 @@ public class UsuarioService {
         usuario.setEmail(usuarioRequest.email());
         usuario.setSenha(usuarioRequest.senha());
         usuario.setTelefone(usuarioRequest.telefone());
-        usuario.setEndereco(endereco);
+
+        if (usuarioRequest.idEndereco() != null){
+            Endereco endereco = enderecoRepository.findById(usuarioRequest.idEndereco())
+                    .orElseThrow(() -> new EntityNotFoundException("Endereco não encontrado"));
+
+            usuario.setEndereco(endereco);
+        }
 
         return usuarioMapper.usuarioToResponse(usuarioRepository.save(usuario));
     }
 
-//    public LogradouroResponse read(UUID id) {
-//        Logradouro logradouro = logradouroRepository.findById(id)
-//                .orElseThrow(() -> new EntityNotFoundException("Logradouro não encontrado"));
-//        return logradouroMapper.logradouroToResponse(logradouro);
-//    }
-//
-//    public Page<LogradouroLista> read(Pageable pageable) {
-//        return logradouroRepository
-//                .findAll(pageable)
-//                .map(logradouroMapper::logradouroToResponseLista);
-//    }
-//
-//    public LogradouroResponse update(UUID id, LogradouroRequest logradouroRequest) {
-//        Logradouro logradouro = logradouroRepository.findById(id)
-//                .orElseThrow(() -> new EntityNotFoundException("Logradouro não encontrado"));
-//        BeanUtils.copyProperties(logradouroRequest, logradouro);
-//        return logradouroMapper.logradouroToResponse(logradouroRepository.save(logradouro));
-//    }
-//
-//    public void delete(UUID id) {
-//        if (!logradouroRepository.existsById(id)) {
-//            throw new EntityNotFoundException("Logradouro não encontrado");
-//        }
-//        logradouroRepository.deleteById(id);
-//    }
+    public UsuarioResponse read(UUID id) {
+        Usuario usuario = usuarioRepository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException("Usuário não encontrado"));
+        return usuarioMapper.usuarioToResponse(usuario);
+    }
+
+    public Page<UsuarioLista> read(Pageable pageable) {
+        return usuarioRepository
+                .findAll(pageable)
+                .map(usuarioMapper::usuarioToResponseLista);
+    }
+
+    public UsuarioResponse update(UUID id, UsuarioRequest usuarioRequest) {
+        Usuario usuario = usuarioRepository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException("Usuário não encontrado"));
+        BeanUtils.copyProperties(usuarioRequest, usuario);
+        return usuarioMapper.usuarioToResponse(usuarioRepository.save(usuario));
+    }
+
+    public void delete(UUID id) {
+        if (!usuarioRepository.existsById(id)) {
+            throw new EntityNotFoundException("Usuário não encontrado");
+        }
+        usuarioRepository.deleteById(id);
+    }
 
 }
